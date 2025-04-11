6d77f9f36a4f78400d8851084edb554d1c7f084b
Yura Stetsyk <yura.stetsyk@kindgeek.com>
2025-04-11 16:58:05 +0300

    Refactor compose theming

--- app/src/main/java/com/example/fxratetracker/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/example/fxratetracker/presentation/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.fxratetracker.ui.theme
+package com.example.fxratetracker.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/example/fxratetracker/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/example/fxratetracker/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.fxratetracker.ui.theme
+package com.example.fxratetracker.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
