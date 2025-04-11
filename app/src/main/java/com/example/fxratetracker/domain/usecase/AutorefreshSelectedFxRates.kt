f4fd8779558966506fbec04eb25219b381bd2d48
Yura Stetsyk <yura.stetsyk@kindgeek.com>
2025-04-11 18:04:13 +0300

    Add AutorefreshSelectedFxRates

---
+++ app/src/main/java/com/example/fxratetracker/domain/usecase/AutorefreshSelectedFxRates.kt
@@ -0,0 +1,50 @@
+package com.example.fxratetracker.domain.usecase
+
+import android.util.Log
+import com.example.fxratetracker.domain.repository.FxRateRepository
+import com.example.fxratetracker.domain.repository.SelectedAssetsRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.isActive
+import java.time.LocalDateTime
+import kotlin.time.Duration
+
+/**
+ * Create flow that emits current fx rate refresh state.
+ * While flow is being collected fx rate will refresh each [period]
+ * */
+class AutorefreshSelectedFxRates(
+    private val dispatcher: CoroutineDispatcher,
+    private val period: Duration,
+    private val fxRateRepository: FxRateRepository,
+    private val selectedAssetsRepository: SelectedAssetsRepository,
+) {
+
+    sealed interface State {
+        data object Stale : State
+        data class Running(val refreshTime: LocalDateTime) : State
+        data class Failed(val exception: Exception) : State
+    }
+
+    operator fun invoke(): Flow<State> = flow {
+        emit(State.Stale)
+
+        while (currentCoroutineContext().isActive) {
+            val selectedCodes = selectedAssetsRepository.getSelectedAssets()
+            fxRateRepository.refreshFxRates(selectedCodes)
+            emit(State.Running(LocalDateTime.now()))
+
+            delay(period)
+        }
+    }.catch { e ->
+        Log.e("AutorefreshSelectedFxRates", "Fx rate autorefresh failed", e)
+        emit(State.Failed(Exception(e)))
+    }
+        .flowOn(dispatcher)
+
+}
