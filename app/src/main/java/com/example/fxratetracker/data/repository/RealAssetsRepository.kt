b7f272b019f650213e752bca6306eec14069597d
zamahaka <zamahaka@gmail.com>
2025-04-12 00:05:05 +0300

    Add failures to AssetsRepository

--- app/src/main/java/com/example/fxratetracker/data/repository/RealAssetsRepository.kt
+++ app/src/main/java/com/example/fxratetracker/data/repository/RealAssetsRepository.kt
@@ -4,8 +4,12 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
+import arrow.core.Either
+import arrow.core.raise.either
 import com.example.fxratetracker.data.remote.service.ExchangeRateService
 import com.example.fxratetracker.domain.model.Asset
+import com.example.fxratetracker.domain.model.Failure
+import com.example.fxratetracker.domain.model.catchUnexpected
 import com.example.fxratetracker.domain.repository.AssetsRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
@@ -18,26 +22,30 @@ class RealAssetsRepository(
     private val json: Json,
 ) : AssetsRepository {
 
+    // TODO: Catch errors
     override fun observeAssets(): Flow<List<Asset>> = store.data.map { preferences ->
         preferences[ASSETS_KEY].takeUnless { it.isNullOrBlank() }
             ?.let { json.decodeFromString(it) }
             ?: emptyList()
     }
 
-    override suspend fun getAssets(): List<Asset> =
-        observeAssets().first().ifEmpty { refreshAssets() }
+    override suspend fun getAssets(): Either<Failure, List<Asset>> = either {
+        observeAssets().first().ifEmpty { refreshAssets().bind() }
+    }
 
-    private suspend fun refreshAssets(): List<Asset> {
-        val response = service.getAssets()
+    private suspend fun refreshAssets(): Either<Failure, List<Asset>> = either {
+        val response = Either.catchUnexpected { service.getAssets() }.bind()
         val assets = response.currencies.map { (code, name) ->
             Asset(code = code, name = name)
         }
 
-        store.edit { preferences ->
-            preferences[ASSETS_KEY] = json.encodeToString(assets)
-        }
+        Either.catchUnexpected {
+            store.edit { preferences ->
+                preferences[ASSETS_KEY] = json.encodeToString(assets)
+            }
+        }.bind()
 
-        return assets
+        assets
     }
 
     private companion object {
