2af374589cca8cb7863d0aae7e7f4af9a66df713
zamahaka <zamahaka@gmail.com>
2025-04-28 14:00:09 +0300

    Add failures to FxRateRepository

--- app/src/main/java/com/example/fxratetracker/domain/repository/FxRateRepository.kt
+++ app/src/main/java/com/example/fxratetracker/domain/repository/FxRateRepository.kt
@@ -1,6 +1,8 @@
 package com.example.fxratetracker.domain.repository
 
+import arrow.core.Either
 import com.example.fxratetracker.domain.model.AssetCode
+import com.example.fxratetracker.domain.model.Failure
 import com.example.fxratetracker.domain.model.FxRateEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -8,8 +10,8 @@ interface FxRateRepository {
 
     fun observeFxRates(assets: Set<AssetCode>): Flow<List<FxRateEntity>>
 
-    suspend fun getFxRates(assets: Set<AssetCode>): List<FxRateEntity>
+    suspend fun getFxRates(assets: Set<AssetCode>): Either<Failure, List<FxRateEntity>>
 
-    suspend fun refreshFxRates(assets: Set<AssetCode>): List<FxRateEntity>
+    suspend fun refreshFxRates(assets: Set<AssetCode>): Either<Failure, List<FxRateEntity>>
 
 }
